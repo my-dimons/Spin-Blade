7b9b902ca21ea432deba156dd8d1f09e8e9f4504
mydimons <mydimons.kb@gmail.com>
2025-07-30 23:10:03 -0700

    bug fix
    fixed a bug where the a upgrades description would be persistant

--- Spin-Blade/Assets/Scripts/Managers/MoneyManager.cs
+++ Spin-Blade/Assets/Scripts/Managers/MoneyManager.cs
@@ -1,3 +1,5 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -7,7 +9,18 @@ public class MoneyManager : MonoBehaviour
     public float moneyMultiplier = 1f;
     public GameObject shopMenu;
     public GameObject moneyText;
-
+    public GameObject upgradeParent;
+    private List<GameObject> upgrades = new List<GameObject>();
+    private void Start()
+    {
+        foreach (Transform child in upgradeParent.transform)
+        {
+            if (child.GetComponent<Upgrade>())
+            {
+                upgrades.Add(child.gameObject);
+            }
+        }
+    }
     // Update is called once per frame
     void Update()
     {
@@ -17,6 +30,15 @@ public class MoneyManager : MonoBehaviour
         {
             Time.timeScale = Time.timeScale == 0 ? 1 : 0; // pause or unpause the game
             shopMenu.SetActive(!shopMenu.activeSelf);
+            
+            foreach (GameObject upgrade in upgrades)
+            {
+                foreach (Transform child in upgrade.transform)
+                {
+                    if (child.CompareTag("UpgradeDsc"))
+                        child.gameObject.SetActive(false);
+                }
+            }
         }
 
         // update money text
