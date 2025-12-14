72cdd06bdc205e98fc31b225b0bb90f05cfc9781
mydimons <mydimons.kb@gmail.com>
2025-12-13 22:06:26 -0800

    Imported UnityUtils's ScriptUtils

---
+++ Spin-Blade/Assets/Script Utils/Audio/BackgroundMusicManager.cs
@@ -0,0 +1,148 @@
+using System.Collections;
+using UnityEngine;
+
+/*
+ *
+ * This script should be created in the main scene, and it will persist through all scenes
+ * 
+ * This script is designed to keep a set of predefined Music tracks playing randomly in the background. 
+ * It does not support dynamic soundtracks, such as changing Music based on the player's location or game events.
+ * But this script does have a few functions to stop and start Music
+ * 
+ * Sometimes this script will bug out if you don't have run in background enabled in Unity (https://discussions.unity.com/t/how-do-you-keep-your-game-running-even-when-you-switch-out-of-it/928)
+ *
+ */
+
+namespace UnityUtils.ScriptUtils.Audio
+{
+    public class BackgroundMusicManager : MonoBehaviour
+    {
+        /// The audio source component used to play the background Music.
+        public AudioSource musicSource;
+
+        /// An array of audio clips representing the available Music tracks.
+        public AudioClip[] musicTracks;
+        [Space(4)]
+
+        /// The current playing track
+        public AudioClip currentPlayingTrack;
+        [Space(8)]
+
+        /// Whether to start playing Music as soon as this object awakes
+        public bool playOnAwake = true;
+
+        /// The duration, in seconds, over which the fade in/out effect occurs.
+        public float fadeTime;
+
+        /// Random cooldown time between songs in milliseconds
+        public Vector2 randomMillisecondCooldownBetweenSongs;
+
+        public static BackgroundMusicManager Instance { get; private set; }
+
+        public Coroutine playingMusicCoroutine;
+
+        void Awake()
+        {
+            if (Instance == null) Instance = this; else Destroy(gameObject);
+
+            DontDestroyOnLoad(gameObject);
+
+            if (playOnAwake)
+                StartContinuousMusic();
+
+        }
+
+        private void Update()
+        {
+            if (musicSource != null)
+                musicSource.volume = AudioManager.CalculateVolumeBasedOnType(1, AudioManager.VolumeType.Music);
+        }
+
+        /// <summary>
+        /// Loops through random songs in <see cref="musicTracks"/> constantly
+        /// </summary>
+        private IEnumerator PlayMusicContinuously()
+        {
+            while (true)
+            {
+                PlaySingleRandomMusicTrack();
+
+                Debug.Log("Clip Started");
+                yield return new WaitWhile(() => musicSource.isPlaying);
+                Debug.Log("Clip Ended");
+
+                Debug.Log("Finished Music Clip");
+
+                float waitTimeUntillNextSong = Random.Range(randomMillisecondCooldownBetweenSongs.x, randomMillisecondCooldownBetweenSongs.y);
+                yield return new WaitForSecondsRealtime(waitTimeUntillNextSong);
+            }
+        }
+
+        /// <summary>
+        /// Stops the <see cref="musicSource"/> from playing Music and looping until <see cref="PlayMusicContinuously"/> is called again (to start looping)
+        /// </summary>
+        public void StopMusic()
+        {
+            if (Instance.musicSource.isPlaying)
+            {
+                Instance.musicSource.Stop();
+
+                if (Instance.playingMusicCoroutine != null)
+                {
+                    Instance.StopCoroutine(Instance.playingMusicCoroutine);
+                    Instance.playingMusicCoroutine = null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Starts to <see cref="PlayMusicContinuously"/> until stopped
+        /// </summary>
+        public void StartContinuousMusic()
+        {
+            bool canPlayMusic = !Instance.musicSource.isPlaying && musicTracks.Length > 0;
+            if (canPlayMusic)
+                playingMusicCoroutine = StartCoroutine(PlayMusicContinuously());
+            else if (musicTracks.Length > 0)
+                Debug.LogWarning("No music tracks in MusicManager.cs");
+            else if (!Instance.musicSource.isPlaying)
+                Debug.LogWarning("Tried starting continuous music but music source is already playing!");
+        }
+
+        /// <summary>
+        /// Plays a Music track on the <see cref="musicSource"/>
+        /// </summary>
+        private void PlayMusicTrack(AudioClip clip)
+        {
+            musicSource.clip = clip;
+            currentPlayingTrack = clip;
+
+            musicSource.Play();
+
+            Debug.Log($"Playing: {clip.name}");
+        }
+
+        /// <summary>
+        /// Plays a random song in <see cref="musicTracks"/> once
+        /// </summary>
+        public void PlaySingleRandomMusicTrack()
+        {
+            PlayMusicTrack(GetRandomSong());
+        }
+
+        /// <summary>
+        /// Plays a specific Music track once
+        /// </summary>
+        public void PlaySpecificMusicTrack(AudioClip clip)
+        {
+            Instance.PlayMusicTrack(clip);
+        }
+
+        /// <returns>Random Music track within <see cref="musicTracks"/></returns>
+        public AudioClip GetRandomSong()
+        {
+            int randomSongTrackIndex = Random.Range(0, musicTracks.Length);
+            return musicTracks[randomSongTrackIndex];
+        }
+    }
+}
---
+++ Spin-Blade/Assets/Script Utils/Audio/SFXManager.cs
@@ -0,0 +1,126 @@
+using UnityEngine;
+
+/*
+ * To setup this script:
+ *    - Create an empty game object in your scene and attach this script to it
+ *    - Make sure you have a AudioManager.cs script in your project (This is used to get volume levels)
+ * 
+ *  To play audio globally (same volume no matter what), use "PlaySfxAudioClip()"
+ *  To play audio spacialy (change volume and channel depending on location), use "PlaySpacialSfxAudioClip()"
+ *  To play audio on a set audio audioSource (such as a character or object), use "PlayClipOnSource()"
+ */
+
+namespace UnityUtils.ScriptUtils.Audio
+{
+    public static class SfxManager
+    {
+        private const float DEFAULT_PITCH_VARIANCE = 0.1f;
+
+        /// <summary>
+        /// Plays an <see cref="AudioClip"/> globally (non-spacialy) at a certain volume with a pitch variance (to feel less repetative).
+        /// </summary>
+        /// <param name="clip"><see cref="AudioClip"/> to play.</param>
+        /// <param name="volume">Playback volume.</param>
+        /// <param name="pitchVariance">
+        /// Random variance to make sound feel less repetative. 
+        /// randomly modifies the pitch of the Music<see cref="AudioSource"/> in a random of 1 - pitchVariance, and 1 + pitchVariance (<see cref="DEFAULT_PITCH_VARIANCE"/> is the default value).
+        /// </param>
+        public static void PlaySfxAudioClip(AudioClip clip, float volume = 1, float pitchVariance = default, AudioManager.VolumeType type = AudioManager.VolumeType.Sfx)
+        {
+            if (pitchVariance == default) pitchVariance = DEFAULT_PITCH_VARIANCE;
+
+            CreateAndPlayAudioClip(clip, volume, pitchVariance, type: type);
+        }
+
+        /// <summary>
+        /// Plays an <see cref="AudioClip"/> globally (non-spacialy) at a certain volume for a set amount of time.
+        /// </summary>
+        /// <param name="clip"><see cref="AudioClip"/> to play</param>
+        /// <param name="time">Specified time for <see cref="AudioClip"/> to play for in seconds.</param>
+        /// <param name="volume">Playback volume.</param>
+        public static void PlayTimedSFXAudioClip(AudioClip clip, float time, float volume = 1, AudioManager.VolumeType type = AudioManager.VolumeType.Sfx)
+        {
+            float clipLength = AudioManager.CalculateClipPitchWithLength(clip.length, time);
+
+            CreateAndPlayAudioClip(clip, volume, pitch: clipLength, type: type);
+        }
+
+        /// <summary>
+        /// Plays an <see cref="AudioClip"/> spacially (Changes volume and L/R volume channel based on location) at a certain volume with a pitch variance (to feel less repetative).
+        /// </summary>
+        /// <param name="clip"><see cref="AudioClip"/> to play.</param>
+        /// <param name="position">Position to play <see cref="AudioClip"/> at.</param>
+        /// <param name="volume">Playback volume.</param>
+        /// <param name="pitchVariance">
+        /// Random variance to make sound feel less repetative. 
+        /// randomly modifies the pitch of the Music<see cref="AudioSource"/> in a random of 1 - pitchVariance, and 1 + pitchVariance (<see cref="DEFAULT_PITCH_VARIANCE"/> is the default value).
+        /// </param>
+        public static void PlaySpacialSfxAudioClip(AudioClip clip, Vector3 position, float volume = 1, float pitchVariance = default, AudioManager.VolumeType type = AudioManager.VolumeType.Sfx)
+        {
+            if (pitchVariance == default) pitchVariance = DEFAULT_PITCH_VARIANCE;
+
+            CreateAndPlayAudioClip(clip, volume, pitchVariance, position: position, type: type);
+        }
+
+        /// <summary>
+        /// Plays an <see cref="AudioClip"/> on an already existing <see cref="AudioSource"/> at a certain volume with a pitch variance (to feel less repetative).
+        /// </summary>
+        /// <param name="volume">Playback volume.</param>
+        /// <param name="audioType">used to get the proper volume, see <see cref="AudioManager.CalculateVolumeBasedOnType(float, AudioManager.VolumeType)"/> to get more info.</param>
+        /// <param name="pitchVariance"> 
+        /// Random variance to make sound feel less repetative. 
+        /// randomly modifies the pitch of the Music<see cref="AudioSource"/> in a random of 1 - pitchVariance, and 1 + pitchVariance (<see cref="DEFAULT_PITCH_VARIANCE"/> is the default value).
+        /// </param>
+        public static void PlayClipOnSource(AudioClip clip, AudioSource source, float volume = 1, float pitchVariance = default, AudioManager.VolumeType audioType = AudioManager.VolumeType.Sfx)
+        {
+            if (pitchVariance == default) pitchVariance = DEFAULT_PITCH_VARIANCE;
+
+            PlayAudioClipOnSource(clip, source, volume, pitchVariance, audioType);
+        }
+
+        private static void CreateAndPlayAudioClip(AudioClip clip, float volume = 1, float pitchVariance = 0, float pitch = default, Vector3 position = default, Transform parent = default, AudioManager.VolumeType type = AudioManager.VolumeType.Sfx)
+        {
+            if (type == default) type = AudioManager.VolumeType.Sfx;
+
+            GameObject temporaryGameObject = new GameObject("Audio Clip (Temporary)");
+            AudioSource audioSource = temporaryGameObject.AddComponent<AudioSource>();
+
+            if (parent == default)
+                temporaryGameObject.transform.parent = null;
+            else
+                temporaryGameObject.transform.parent = parent;
+
+            // set to Global or spacial audio
+            if (position == default)
+            {
+                temporaryGameObject.transform.position = Camera.main.transform.position;
+                audioSource.spatialBlend = 0f; // 2D sound
+            }
+            else
+            {
+                temporaryGameObject.transform.position = position;
+                audioSource.spatialBlend = 1f; // 3D sound
+            }
+
+            if (pitch == default)
+                PlayAudioClipOnSource(clip, audioSource, volume, pitchVariance, type);
+            else
+                PlayAudioClipOnSource(clip, audioSource, volume, 0f, type, pitch);
+
+            float destroyTime = AudioManager.CalculateClipLength(clip.length, audioSource.pitch);
+            Object.Destroy(temporaryGameObject, destroyTime);
+        }
+
+        private static void PlayAudioClipOnSource(AudioClip audioClip, AudioSource audioSource, float volume, float pitchVariance = default, AudioManager.VolumeType audioType = AudioManager.VolumeType.Sfx, float pitch = default)
+        {
+            audioSource.clip = audioClip;
+            audioSource.volume = AudioManager.CalculateVolumeBasedOnType(volume, audioType);
+
+            float randomPitch = AudioManager.CalculatePitchVariance(pitchVariance);
+            float usedPitch = pitch == default ? randomPitch : pitch;
+            audioSource.pitch = usedPitch;
+
+            audioSource.Play();
+        }
+    }
+}
