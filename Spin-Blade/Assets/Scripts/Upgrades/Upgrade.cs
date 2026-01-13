9aa929801018da297d873b7eb91f7253cf355375
mydimons <mydimons.kb@gmail.com>
2026-01-13 09:58:08 -0800

    buy particles take color from currency

--- Spin-Blade/Assets/Scripts/Upgrades/Upgrade.cs
+++ Spin-Blade/Assets/Scripts/Upgrades/Upgrade.cs
@@ -406,7 +406,7 @@ public class Upgrade : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 
         SfxManager.PlaySfxAudioClip(buySound, 0.8f);
         Camera.main.GetComponent<CameraScript>().ScreenshakeFunction(0.1f);
-        ParticleSpawner.SpawnBurstParticle(buyParticlesPrefab, transform.position, transform, fullyBoughtOutlineColor);
+        ParticleSpawner.SpawnBurstParticle(buyParticlesPrefab, transform.position, transform, moneyManager.GetCurrencyColor(priceCurrencyType));
 
         foreach (IUpgrade upgrade in GetComponents<IUpgrade>())
         {
