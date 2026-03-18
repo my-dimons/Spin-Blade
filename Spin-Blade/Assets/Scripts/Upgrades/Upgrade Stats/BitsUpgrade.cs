4701175c7ef83c67f62be321c7bc9405aaf9225f
mydimons <mydimons.kb@gmail.com>
2026-03-18 14:55:03 -0700

    bits converter button uses bits multiplier

--- Spin-Blade/Assets/Scripts/Upgrades/Upgrade Stats/BitsUpgrade.cs
+++ Spin-Blade/Assets/Scripts/Upgrades/Upgrade Stats/BitsUpgrade.cs
@@ -1,17 +1,24 @@
 using UnityEngine;
 
 public class BitsUpgrade : MonoBehaviour, IUpgrade {
-	public bool unlockBits;
-	public float bitsMultiplierIncrease;
-	public float giveBits;
+  public bool unlockBits;
+  public float bitsMultiplierIncrease;
+  [Space(8)]
+  public float giveBits;
+  public bool updateBitsText;
 
-	public void ApplyUpgrade() {
-		MoneyManager moneyManager = MoneyManager.Instance;
+  private void Update() {
+    if (TryGetComponent(out Upgrade upgrade) && updateBitsText) {
+      upgrade.description = $"gives +{MoneyManager.Instance.CalculateCurrency(giveBits, MoneyManager.Currency.bits)} bits";
+    }
+  }
+  public void ApplyUpgrade() {
+    MoneyManager moneyManager = MoneyManager.Instance;
 
-		if (!moneyManager.bitsUnlocked)
-			moneyManager.bitsUnlocked = unlockBits;
+    if (!moneyManager.bitsUnlocked)
+      moneyManager.bitsUnlocked = unlockBits;
 
-		moneyManager.bitsMultiplier += bitsMultiplierIncrease;
-		moneyManager.bits += giveBits;
-	}
+    moneyManager.bitsMultiplier += bitsMultiplierIncrease;
+    moneyManager.bits += MoneyManager.Instance.CalculateCurrency(giveBits, MoneyManager.Currency.bits);
+  }
 }
