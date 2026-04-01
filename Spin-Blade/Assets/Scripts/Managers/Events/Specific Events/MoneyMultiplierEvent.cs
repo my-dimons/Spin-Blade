cac411ecd0d4c31058011031995a2fd2e69aea71
mydimons <mydimons.kb@gmail.com>
2026-03-31 23:32:09 -0700

    key art

--- Spin-Blade/Assets/Scripts/Managers/Events/Specific Events/MoneyMultiplierEvent.cs
+++ Spin-Blade/Assets/Scripts/Managers/Events/Specific Events/MoneyMultiplierEvent.cs
@@ -3,42 +3,42 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "MoneyMultiplierEvent", menuName = "ScriptableObjects/Events/MoneyMultiplierEvent")]
 public class MoneyMultiplierEvent : Event {
-	public float moneyIncrease;
-	public float duration;
+  public float moneyIncrease;
+  public float duration;
 
-	[Space(10)]
+  [Space(10)]
 
-	public bool enabled = true;
-	public string eventName = "MoneyMultiplierEvent";
+  public bool enabled = true;
+  public string eventName = "MoneyMultiplierEvent";
 
-	public override void ApplyEvent() {
-		EventManager.Instance.StartCoroutine(Event(moneyIncrease, duration));
-	}
+  public override void ApplyEvent() {
+    EventManager.Instance.StartCoroutine(Event(moneyIncrease, duration));
+  }
 
-	public override bool IsEnabled() {
-		return enabled;
-	}
+  public override bool IsEnabled() {
+    return enabled;
+  }
 
-	public override string GetEventName() {
-		return eventName;
-	}
+  public override string GetEventName() {
+    return eventName;
+  }
 
-	public override string GetEventPopupTextString() {
-		return "+" + moneyIncrease + "x Money Multiplier";
-	}
+  public override string GetEventPopupTextString() {
+    return "+" + moneyIncrease + "x Money Multiplier";
+  }
 
-	IEnumerator Event(float moneyMultiplier, float duration) {
-		EventManager eventManager = EventManager.Instance;
-		MoneyManager moneyManager = MoneyManager.Instance;
+  IEnumerator Event(float moneyMultiplier, float duration) {
+    EventManager eventManager = EventManager.Instance;
+    MoneyManager moneyManager = MoneyManager.Instance;
 
-		eventManager.StartCoroutine(eventManager.EnableEventText(GetEventPopupTextString(), duration));
-		eventManager.eventHappening = true;
+    eventManager.StartCoroutine(eventManager.EnableEventText(GetEventPopupTextString(), duration));
+    eventManager.eventHappening = true;
 
-		moneyManager.moneyMultiplier += moneyMultiplier;
+    moneyManager.moneyMultiplier += moneyMultiplier;
 
-		yield return new WaitForSeconds(duration);
+    yield return new WaitForSeconds(duration);
 
-		moneyManager.moneyMultiplier -= moneyMultiplier;
-		eventManager.eventHappening = false;
-	}
+    moneyManager.moneyMultiplier -= moneyMultiplier;
+    eventManager.eventHappening = false;
+  }
 }
